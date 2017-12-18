0139357a2da452b2d0d7fd166de895b14fd97bba
Phillip Nathaniel Craig <34584416+phillipncraig@users.noreply.github.com>
2017-12-18 15:08:55 -0800

    Optional exercises commit

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,8 +2,7 @@
 Exercise #1
 Create a variable named "numbers" and assign an empty array to it.
 */
-var numbers;
-console.log('hello world!');
+var numbers = [];
 
 /*
 Exercise #2
@@ -11,16 +10,15 @@ Using the loop provided for you below, use the array push method to insert the n
 */
 
 for(var i=0;i<10;i++){
-    //use the array push() method here.
+    numbers.push(i);//use the array push() method here.
 }
-
 
 /*
 Exercise #3
 Test that you used the array push method correctly by console logging the first item in the array. Console log the last item in the array afterwards.
 */
-console.log();
-console.log();
+console.log(numbers[0]);
+console.log(numbers[9]);
 
 
 /*
@@ -28,6 +26,8 @@ Exercise #5
 Create an empty object and assign it to the variable car below.
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
-var car;
-
+var car = {
+    colour:"black",
+}
+ console.log(car.colour);
 
---
+++ assignment_1/test.js
@@ -0,0 +1,39 @@
+var name = "Phill"; //Optional Exercise 1
+console.log(name);
+console.log("Hello"+" "+name);
+
+var x = 1125; //Optional Exercise 2
+x = x%2
+console.log(x);
+
+var a = true //Optional Exercise 3
+var b = false
+var c = true
+var d = a || b || c
+console.log(d);
+
+var a = true //Optional Exercise 4
+var b = false
+var c = true
+var d = a && b && c
+console.log(d);
+
+var answer1 = 10-3*5/4+202%3; //Optional Exercise 5
+var answer2 = 10-(3*(5/4)+(202%3));
+console.log(answer1);
+console.log(answer2);
+
+var a = 12; //Optional Exercise 6
+a = String(a);
+
+var b = "113";
+b = Number(b);
+
+var c = "1.12356"
+c = Number(c);
+
+var d = "Hello World"
+d = Number(d);
+console.log(d);
+
+console.log('34'+5);
