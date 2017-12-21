35b87596b5aa279fa57f084247affc262305382a
Phillip Nathaniel Craig <34584416+phillipncraig@users.noreply.github.com>
2017-12-21 11:44:30 -0800

    Optional and submission exercises within (different in the document and the submission instructions)

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,5 +1,53 @@
-// Exercise #1
+
+/* Submission Exercise #1
+Write a loop that logs the following triangle to the console:
+(Diving Deeper: Are you doing console.log() multiple times for each line? Can you do it with one call for all 7 lines?)
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+data = [];
+for (var i=1; i<8; i++) {
+    var a = "#"
+    if (i<8) {
+        a = a.repeat(i);
+        data.push(a);
+    }
+}
+var output = data.join("\n");
+console.log(output);
+
+/* Submission Exercise #2
+Write a function that checks whether a number is odd or even. (Hint: Read about the modulo operator - %)
+*/
+
+function isEven(number) {
+    if (isNaN(number)) {
+        console.log("sure that's that a number?")
+    }
+    else if (number%2 == 0) {
+        console.log("the number is even!")
+    }
+    else {
+        console.log("the number is odd!")
+    }
+}
+
+isEven(4)
+isEven(33)
+isEven("test")
+
+
+// OPTIONAL Exercise #1
 // Write your solution below
+
 for (var i = 1; i <= 100; i++) {
     if (i%3 !== 0 && i%5 !== 0) {
     console.log(i)
@@ -18,8 +66,6 @@ for (var i = 1; i <= 100; i++) {
     }
 }
 
-
-
 /* 
 Write your explanation for Exercise #1 here
 1. Started out by creating a for loop that spits out numbers 1-100. Choose 'for' as opposed to 'switch' because I'm intending to evaluate the values as boolean operation. 
@@ -32,8 +78,9 @@ Write your explanation for Exercise #1 here
 
 
 
-// Exercise #2
+// OPTIONAL Exercise #2
 // Write your solution below
+
 var a = "# "
 var b = " #"
 
@@ -57,3 +104,5 @@ Write your explanation for Exercise #2 here
 4. Choose while not an if/else because this is based on pattern picking the a or b values, and to mix it up a bit. Actually maybe that was the prompt to choose switch rather than if/else? 
 5. Easy function testing the even/odd counter variable, and alternating to post a 4x multiple of the a or b strings for the pattern grid
 */
+
+
